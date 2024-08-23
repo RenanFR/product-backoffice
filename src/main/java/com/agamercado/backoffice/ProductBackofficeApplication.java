035c449c95642ddcd246078e79070cf48f41d4f8
renan.rodrigues.glo <renan.rodrigues.3@globalhitss.com.br>
2024-08-23 07:23:47 -0300

    first commit

---
+++ src/main/java/com/agamercado/backoffice/ProductBackofficeApplication.java
@@ -0,0 +1,13 @@
+package com.agamercado.backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductBackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductBackofficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agamercado/backoffice/ProductBackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agamercado.backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductBackofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
